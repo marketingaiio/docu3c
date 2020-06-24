41ea40074984096b2b4ad945385d11a574ebdd78
Shyamala Ilangovan <shyamala.it@hotmail.com>
2020-06-24 16:53:52 +0530

    Add Files

---
+++ docu3c/Controllers/LoginController.cs
@@ -0,0 +1,98 @@
+﻿using docu3c.TableHandling;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace docu3c.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        private TableManager TableManagerObj = new TableManager("UserRegistration");
+        string role = string.Empty;
+        string userName = string.Empty;
+        // GET: Login
+
+    //    [SessionFilter(Enable = false)]
+        public ActionResult Login()
+        {
+            return View();
+
+        }
+
+ //       [SessionFilter(Enable = false)]
+                [HttpPost]
+        public ActionResult Login(FormCollection formInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                string userEmailID = formInfo["Emailid"] == "" ? null : formInfo["Emailid"];
+                string password = formInfo["password"] == "" ? null : formInfo["password"];
+
+
+                if (!string.IsNullOrEmpty(userEmailID) && !string.IsNullOrEmpty(password))
+                {
+                    if (isValid(userEmailID, password))
+                    {
+                        role = role.Replace(" ", "");
+                        Session["UserEmailID"] = userEmailID;
+                        Session["UserName"] = userName;
+                        Session["Role"] = role;
+                        // return RedirectToAction("Dashboard", "Document");
+                        //if (role.Contains("ChiefExecutiveOfficer"))
+                        //{
+                        return RedirectToAction("Index", "Home");
+                        //}
+                        //else if (role.Contains("FinancialAdvisor"))
+                        //{
+                        //    return RedirectToAction("FinancialAdvisor", "Home");
+                        //}
+                        //else if (role.Contains("Admin"))
+                        //{
+                        //    return RedirectToAction("Get", "Home");
+
+
+                        //}
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("CustomError", "Please enter the username and password");
+                    }
+
+
+                }
+                else
+                {
+                    ModelState.AddModelError("CustomError", "Please enter the username and password");
+                }
+            }
+            return View();
+        }
+
+
+        private bool isValid(string userEmailID, string password)
+        {
+            var Users = TableManagerObj.RetrieveEntity<User>("EmailId eq '" + userEmailID + "'");
+            var User = Users.Where(v => v.CPassword == password).FirstOrDefault();
+            if (User != null)
+            {
+
+
+                role = User.RoleType;
+                userName = User.FirstName + " " + User.LastName;
+
+            }
+            return User != null ? true : false;
+        }
+        public ActionResult Dashboard()
+        {
+            if (Session["UserName"] != null && Session["Role"] != null && Session["UserEmailID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ docu3c/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System.ComponentModel.DataAnnotations;
+
+namespace docu3c
+{
+    public class User : TableEntity
+    {
+        public User() { }
+        [Key]
+        public int UserId { set; get; }
+
+        [StringLength(100)]
+        [Display(Description = "First Name")]
+        [Required(ErrorMessage = "Enter First Name")]
+        [RegularExpression(@"^[a-zA-Z0-9\s]*$", ErrorMessage = "Enter valid First Name.")]
+        public string FirstName { set; get; }
+        [StringLength(100)]
+        [Display(Description = "Last Name")]
+        [Required(ErrorMessage = "Enter Last Name")]
+        [RegularExpression(@"^[a-zA-Z0-9\-_\s]*$", ErrorMessage = "Enter valid Last Name.")]
+        public string LastName { set; get; }
+
+        [StringLength(100)]
+        [Display(Description = "EmailID")]
+        [Required(ErrorMessage = "Enter EmailID")]
+        [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", ErrorMessage = "Please enter a valid e-mail adress")]
+        public string EmailId { set; get; }
+
+        [StringLength(100)]
+        [Display(Description = "Password")]
+        [Required(ErrorMessage = "Enter the Password")]
+        public string CPassword { set; get; }
+
+        [Required(ErrorMessage = "Select any Role Type")]
+        public string RoleType { set; get; }
+
+
+        [StringLength(200)]
+        [Display(Description = "Organization Name")]
+        [Required(ErrorMessage = "Enter Organization Name")]
+        public string OrganizationName { set; get; }
+
+        [StringLength(250)]
+        [Display(Description = "Address")]
+        [Required(ErrorMessage = "Enter the Address")]
+        public string CAddress { set; get; }
+
+        [Display(Description = "PostalCode")]
+        [RegularExpression(@"^(?!00000)[0-9]{5,5}$", ErrorMessage = "Please enter the valid Postal Code")]
+        public int? PostalCode { set; get; }
+
+        [Display(Description = "PhoneNo")]
+        [RegularExpression(@"^[0-9]{5,12}$", ErrorMessage = "Please enter the valid Phone Number ")]
+        public int? PhoneNo { set; get; }
+
+        [Display(Description = "Telephone No")]
+        [RegularExpression(@"^[0-9]{5,12}$", ErrorMessage = "Please enter the valid Telephone Number ")]
+        public int? TeleNo { set; get; }
+        public bool IsActive { set; get; }
+
+    }
+}
